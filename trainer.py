2d882b499145e01d36f5e525d2c715364a3fc879
Mohammed Sharieff <54046329+marscolony2040@users.noreply.github.com>
2026-03-25 14:10:18 -0400

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,3 +1,4 @@
+# Loading necessary libraries
 import os
 from PIL import Image
 import numpy as np
@@ -8,9 +9,11 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 import time
 
+# Initialize Timestamp Function to measure Duration
 print("Brain Scan Trainer")
 stamp = lambda: int(time.time())
 
+# Load 52 Images and transform them into PyTorch Tensors
 nlen = 52
 img = [Image.open(f'inputs/{i}.png') for i in range(nlen)]
 
@@ -22,10 +25,14 @@ transform = transforms.Compose([
 
 
 inputs = [transform(u) for u in img]
+
+# Load the image prediction outputs
 outputs = list(map(lambda tb: int(tb.replace('\n','').split('.')[0]), open('output.txt','r').readlines()))
 
+# Classifications on Brain Status
 choices = {0:'normal',1:'tumor',2:'truama',3:'schizophrenic'}
 
+# Convolutional Neural Network which is used to predict and classify brain images
 class Predict(nn.Module):
     def __init__(self):
         super(Predict, self).__init__()
@@ -47,16 +54,20 @@ class Predict(nn.Module):
         x = x.flatten(1)
         return self.ending(x)
             
-
+# Load th model and declare the loss function and optimizer
 model = Predict()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.0001)
 epochs = 300
 
+# Train model on images and outputs
 print("Let the training begin!")
+
 for epoch in range(1, epochs+1):
     rmse = 0
     t0 = stamp()
+    
+    # Loop through each image to train model
     for i in range(nlen):
         tout = model(inputs[i].unsqueeze(0))
         tyes = torch.tensor([i], dtype=torch.long)
@@ -65,8 +76,10 @@ for epoch in range(1, epochs+1):
         loss.backward()
         optimizer.step()
         rmse += loss.item()
+        
     t1 = stamp()
     print(f'Epoch: {epoch} | Loss: {rmse} | Duration: {t1 - t0}')
 
+# Save model after training
 torch.save(model, 'brain_model.pth')
 print("Training has ended, model has been saved")
